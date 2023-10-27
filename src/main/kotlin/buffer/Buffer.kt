f6bd3f74693862e288d7c61e0391b406afc13035
manpreet <manpreetrahal27@gmail.com>
2023-10-26 23:36:20 -0600

    Add Buffer class.

---
+++ src/main/kotlin/buffer/Buffer.kt
@@ -0,0 +1,53 @@
+package buffer
+
+import filemanager.BlockId
+import filemanager.FileManager
+import filemanager.Page
+import log.LogManager
+
+/**
+ * Represents a buffer, that contains a [Page] and other data such as
+ * whether the [Page] is pinned or not.
+ */
+class Buffer(private val fileManager: FileManager, private val logManager: LogManager) {
+
+    val contents = Page(fileManager.blockSize)
+
+    private var pins = 0
+
+    private var block: BlockId? = null
+
+    var transactionNumber = -1
+
+    private var lsn = -1
+
+    fun isPinned() = pins > 0
+
+    fun setModified(transactionNumber: Int, lsn: Int) {
+        this.transactionNumber = transactionNumber
+        if (lsn >= 0) {
+            this.lsn = lsn
+        }
+    }
+
+    /**
+     * Flushes the previously assigned block if dirty,
+     * and assigns a new block
+     */
+    fun assignToBlock(blockId: BlockId) {
+        flush()
+        block = blockId
+        fileManager.read(blockId, contents)
+        pins = 0
+    }
+
+    private fun flush() {
+        if (transactionNumber >= 0) {
+            logManager.flush(lsn)
+            fileManager.write(block!!, contents)
+            transactionNumber--
+            lsn--
+        }
+    }
+
+}
---
+++ src/main/kotlin/buffer/BufferManager.kt
@@ -0,0 +1,24 @@
+package buffer
+
+import filemanager.BlockId
+import filemanager.FileManager
+import log.LogManager
+
+class BufferManager(val fileManager: FileManager, val logManager: LogManager, val numBuffers: Int) {
+
+    fun pin(block: BlockId): Buffer {
+        TODO()
+    }
+
+    fun unpin(buffer: Buffer) {
+
+    }
+
+    fun available(): Int {
+        TODO()
+    }
+
+    fun flushAll(transactionNumber: Int) {
+
+    }
+}
